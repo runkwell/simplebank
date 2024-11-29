a615e37da6dfccc67b4b2e398d719ff2a6d108a6
runkwell <runkwell@gmail.com>
2024-11-30 04:04:03 +0700

    initial commit

---
+++ sqlc/main_test.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var TestQueries = Queries
+
+func TestMain(m = testing.M){
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("can not connect to db:",err)
+	}
+	
+	TestQueries = New(conn)
+
+	os.Exit(m.Run())
+}
